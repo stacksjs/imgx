2086ffe22f35e1122f92de903a6001b4a91fd290
Chris <chrisbreuer93@gmail.com>
2026-04-29 17:11:05 -0700

    chore: wip

--- packages/ts-images/src/ts-potrace.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'ts-potrace' {
-  export const Potrace: any
-  export const Posterizer: any
-  export function trace(_data: any, _options?: any): Promise<any>
-  export function posterize(_data: any, _options?: any): Promise<any>
-}
